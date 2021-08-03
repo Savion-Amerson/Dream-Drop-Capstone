b5d26c40ef0cc78bf545673ea598af979de1e9ec
Savion Amerson <savionamerson@gmail.com>
2021-08-02 19:52:03 -0500

    Converted to SPA

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+import html from "html-literal";
+
+export default () => html`
+  <footer class="footer">
+    <h1>BUILT BY SAVION AMERSON</h1>
+  </footer>
+`;
---
+++ components/Nav.js
@@ -0,0 +1,18 @@
+import html from "html-literal";
+
+export default () => html`
+  <nav class="navbar">
+    <h1 class="logo">DREAM DROP</h1>
+    <div class="nav-buttons">
+      <button class="pfp-button">
+        <i class="pfp material-icons">person</i>
+      </button>
+      <div class="pfp-dropdown">
+        <ul class="pfp-menu-options">
+          <li>LOGIN</li>
+          <li>REGISTER</li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+`;
