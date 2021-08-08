83caa39e284d865fc620227ef92eb09b73dce6fc
Savion Amerson <savionamerson@gmail.com>
2021-08-08 14:57:13 -0500

    atempted to ad nav toggle function

--- components/Nav.js
+++ components/Nav.js
@@ -5,10 +5,10 @@ export default () => html`
     <h1 class="logo">DREAM DROP</h1>
     <div class="nav-buttons">
       <button class="pfp-button">
-        <i class="pfp material-icons">person</i>
+        <i class="pfp material-icons" id="userIcon" class="toggle">person</i>
       </button>
       <div class="pfp-dropdown">
-        <ul class="pfp-menu-options">
+        <ul class="pfp-menu-options" class="toggle-content">
           <li>LOGIN</li>
           <li>REGISTER</li>
         </ul>
--- index.js
+++ index.js
@@ -7,79 +7,40 @@ function render() {
     ${Main()}
     ${Footer()}
   `;
+  addEventListener();
 }
 
 render();
 
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+function addEventListener() {
+  // add menu toggle to bars icon in nav bar
+  //document.querySelector(".fa-bars").addEventListener("click", () => {
+  //  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  //});
 
-// array of pictures for gallery
-const dogPictures = [
-  {
-    url:
-      "https://images.unsplash.com/photo-1505628346881-b72b27e84530?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    title: "sunglass doggo"
-  },
-  {
-    url:
-      "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    title: "mlep"
-  },
-  {
-    url:
-      "https://images.unsplash.com/photo-1514984879728-be0aff75a6e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    title: "good boy with stick"
-  },
-  {
-    url:
-      "https://images.unsplash.com/photo-1477936432016-8172ed08637e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    title: "black camo doggo"
-  },
-  {
-    url:
-      "https://images.unsplash.com/photo-1567529684892-09290a1b2d05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    title: "happy boi"
-  },
-  {
-    url:
-      "https://images.unsplash.com/photo-1529429617124-95b109e86bb8?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    title: "eski"
-  },
-  {
-    url:
-      "https://images.unsplash.com/photo-1516371535707-512a1e83bb9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    title: "doin a confuse"
-  },
-  {
-    url:
-      "https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    title: "happy little jump boi"
-  },
-  {
-    url:
-      "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    title: "runnin with the dogs"
-  }
-];
+  // Select userIcon
+  let show = function(e) {
+    e.classList.addEventListener("is-visible");
+  };
+  let hide = function(e) {
+    e.classList.remove("is-visible");
+  };
+  let toggle = function(e) {
+    e.classList.toggle("is-visible");
+  };
 
-// populating gallery with pictures
-const gallerySection = document.querySelector("#gallery");
-dogPictures.forEach(pic => {
-  let img = document.createElement("img");
-  img.src = pic.url;
-  img.alt = pic.title;
-  gallerySection.appendChild(img);
-});
+  const userIcon = document.getElementById("userIcon");
+  userIcon.addEventListener(
+    "click",
+    e => {
+      console.log("you just moused over me");
+      if (!e.target.classList.contains("toggle")) return;
 
-// handle form submission
-document.querySelector("form").addEventListener("submit", event => {
-  event.preventDefault();
-  Array.from(event.target.elements).forEach(el => {
-    console.log("Input Type: ", el.type);
-    console.log("Name: ", el.name);
-    console.log("Value: ", el.value);
-  });
-});
+      let content = document.querySelector(e.target.hash);
+      if (!content) return;
+
+      toggle(content);
+    },
+    false
+  );
+}
