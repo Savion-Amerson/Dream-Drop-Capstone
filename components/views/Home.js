df16f4d12b8f6f97659a313e5e263ddbfda59983
Savion Amerson <savionamerson@gmail.com>
2021-08-11 18:24:16 -0500

    Converted to stateful

---
+++ components/views/Home.js
@@ -0,0 +1,78 @@
+import html from "html-literal";
+
+export default () => html`
+  <main class="main-content">
+    <div class="welcome">
+      <h1>WELCOME TO DREAM DROP!</h1>
+    </div>
+
+    <div class="content-container">
+      <div class="dream-card">
+        <h1>Dream Title</h1>
+        <div class="dream-preview">
+          Preview of dream
+        </div>
+      </div>
+
+      <div class="dream-card">
+        <h1>Dream Title</h1>
+        <div class="dream-preview">
+          Preview of dream
+        </div>
+      </div>
+
+      <div class="dream-card">
+        <h1>Dream Title</h1>
+        <div class="dream-preview">
+          Preview of dream
+        </div>
+      </div>
+
+      <div class="dream-card">
+        <h1>Dream Title</h1>
+        <div class="dream-preview">
+          Preview of dream
+        </div>
+      </div>
+
+      <div class="dream-card">
+        <h1>Dream Title</h1>
+        <div class="dream-preview">
+          Preview of dream
+        </div>
+      </div>
+
+      <div class="dream-card">
+        <h1>Dream Title</h1>
+        <div class="dream-preview">
+          Preview of dream
+        </div>
+      </div>
+
+      <div class="dream-card">
+        <h1>Dream Title</h1>
+        <div class="dream-preview">
+          Preview of dream
+        </div>
+      </div>
+
+      <div class="dream-card">
+        <h1>Dream Title</h1>
+        <div class="dream-preview">
+          Preview of dream
+        </div>
+      </div>
+
+      <div class="dream-card">
+        <h1>Dream Title</h1>
+        <div class="dream-preview">
+          Preview of dream
+        </div>
+      </div>
+    </div>
+    <div class="page-btns">
+      <button class="page-back-btn" type="button"><</button>
+      <button class="page-fwd-btn" type="button">></button>
+    </div>
+  </main>
+`;
--- index.js
+++ index.js
@@ -1,46 +1,73 @@
-import { Header, Nav, Main, Footer } from "./components";
+import { Nav, Main, Footer } from "./components";
+import * as state from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+import axios from "axios";
 
-function render() {
+function render(st = state.Home) {
   document.querySelector("#root").innerHTML = `
-    ${Header()}
-    ${Nav()}
-    ${Main()}
+    ${Nav(state.Links)}
+    ${Main(st)}
     ${Footer()}
-  `;
-  addEventListener();
+    `;
+  router.updatePageLinks();
 }
 
-render();
+render(state.Home);
 
-function addEventListener() {
-  // add menu toggle to bars icon in nav bar
-  //document.querySelector(".fa-bars").addEventListener("click", () => {
-  //  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-  //});
+const router = new Navigo(window.location.origin);
 
-  // Select userIcon
-  let show = function(e) {
-    e.classList.addEventListener("is-visible");
-  };
-  let hide = function(e) {
-    e.classList.remove("is-visible");
-  };
-  let toggle = function(e) {
-    e.classList.toggle("is-visible");
-  };
+router.hooks({
+  before: (done, params) => {
+    const page =
+      // eslint-disable-next-line no-prototype-builtins
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
 
-  const userIcon = document.getElementById("userIcon");
-  userIcon.addEventListener(
-    "click",
-    e => {
-      console.log("you just moused over me");
-      if (!e.target.classList.contains("toggle")) return;
+    switch (page) {
+      case "Blog":
+        state.Blog.posts = [];
+        axios
+          .get("https://jsonplaceholder.typicode.com/posts/")
+          .then(response => {
+            response.data.forEach(post => {
+              state.Blog.posts.push(post);
+            });
+            done();
+            // console.log(state.Blog.posts);
+          })
+          .catch(err => console.log(err));
+        break;
 
-      let content = document.querySelector(e.target.hash);
-      if (!content) return;
+      case "Home":
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=fbb30b5d6cf8e164ed522e5082b49064&q=st.%20louis`
+          )
+          .then(response => {
+            state.Home.weather = {};
+            // console.log(response, state.Home.weather);
+            state.Home.weather.city = response.data.name;
+            state.Home.weather.temp = response.data.main.temp;
+            state.Home.weather.feelsLike = response.data.main.feels_like;
+            state.Home.weather.humidity = response.data.main.humidity;
+            state.Home.weather.description =
+              response.data.weather[0]["description"];
+            done();
+          })
+          .catch(err => console.log(err));
+        break;
 
-      toggle(content);
-    },
-    false
-  );
-}
+      default:
+        done();
+    }
+  }
+});
+
+router
+  .on({
+    "/": () => render(state.Home),
+    ":page": params => render(state[capitalize(params.page)])
+  })
+  .resolve();
