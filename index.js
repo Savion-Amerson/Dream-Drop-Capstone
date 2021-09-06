d54e0dda0fe0da8c58c8019605ec73884d4f361c
Savion Amerson <savionamerson@gmail.com>
2021-09-06 16:02:09 -0500

    Display all back end data onto the front end

--- index.js
+++ index.js
@@ -46,6 +46,48 @@ function addEventListeners(st) {
         });
     });
   }
-}
 
-// render(state.Home);
+  axios
+    .get(`${process.env.API}/dreams`)
+    .then(response => {
+      let dreamBlockAmt = response.data.length - 1;
+      console.log(dreamBlockAmt);
+
+      for (let i = dreamBlockAmt; i > 0; i--) {
+        document.querySelector(
+          "#mainhomecontent"
+        ).innerHTML += `<a href="/Dream/${response.data[i]._id}"><div class="dream-card" id="dreamcard">
+          <h1 id="dreamtitle">${response.data[i].title}</h1>
+             <p class="dreamdataid">${response.data[i]._id}</P>
+         <div class="dream-preview" id="dreampreview">
+          ${response.data[i].content}
+         </div>
+      </div></a>`;
+      }
+    })
+
+    .catch(error => {
+      console.log("It puked", error);
+    });
+
+  if (window.location.href.indexOf("6") > -1) {
+    var dreamURL = window.location.href.slice(-24);
+    window.sessionStorage.setItem("current", dreamURL);
+    router.navigate("/dream");
+  }
+
+  if (st.view === "Dream") {
+    axios
+      .get(
+        `${process.env.API}/dreams/${window.sessionStorage.getItem("current")}`
+      )
+      .then(response => {
+        document.querySelector("#dream-post-body").innerHTML += `
+        <h1 class="dream-post-title">${response.data.title}</h1>
+        <p>${response.data.content}</p>`;
+      })
+      .catch(error => {
+        console.log("It puked", error);
+      });
+  }
+}
